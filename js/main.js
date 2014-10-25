7ab20dde6f515737bb0157f5b157fec55f13fbf8
bowmessage <bowmessage@gmail.com>
2014-10-25 17:34:12 -0500

    ASDFLJKASDF

--- js/main.js
+++ js/main.js
@@ -1,26 +1,3 @@
-var hasOAUTH = false;
-var allImg = [];
-function shuffle(array) {
-    var counter = array.length, temp, index;
-
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        index = Math.floor(Math.random() * counter);
-
-        // Decrease counter by 1
-        counter--;
-
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
-
-    return array;
-}
-
-
 function init() {
     window.map = new OpenLayers.Map("basicMap");
     var mapnik = new OpenLayers.Layer.OSM();
@@ -91,7 +68,7 @@ function init() {
     OAuth.initialize('AlrP4jjCIXkqVpJE_tZxvuqsF58')
     OAuth.popup('flickr', {}, function(error, result) {
         console.log(result)
-        var hasOAUTH = true;
+
 
         setPhotos();
 
@@ -143,46 +120,24 @@ app.controller('mainCtrl', ['$scope',
 
 function setPhotos() {
     $("#rotatingImages").empty();
-    if (hasOAUTH) {
-        if (allImg.length == 0) {
-            $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a4dbe979f03ec20953a445250a5af87f&bbox=-30%2C-30%2C-7%2C-7&format=json&nojsoncallback=1&auth_token=72157648555864100-757f7a945585930c&api_sig=d09942fbdb4fb123409e37331a909412", function(data) {
-                console.log(data);
-                var lim = 7;
-                var i = 0;
-                allImg = data.photos.photo
-                $.each(allImg, function(k, v) {
-                    if (i < lim) {
-                        var j = k + 2;
-                        var src = "https://farm" + v.farm + ".staticflickr.com/" + v.server + "/" + v.id + "_" + v.secret + "_b.jpg";
-                        $("#rotatingImages").append($(new Image()).attr('src', src).attr('class', 'threed').css('border', '1px solid black').css('transform', 'rotateY(' + (j * 35) + 'deg) translate3d(' + (Math.round(Math.sin(j * Math.PI / 8) * 800)) + 'px, -200px, ' + (Math.round(Math.cos(j * Math.PI / 8) * 800)) + 'px) '));
+    $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a4dbe979f03ec20953a445250a5af87f&bbox=-30%2C-30%2C-7%2C-7&format=json&nojsoncallback=1&auth_token=72157648555864100-757f7a945585930c&api_sig=d09942fbdb4fb123409e37331a909412", function(data) {
+        console.log(data);
+        var lim = 7;
+        var i = 0;
+        $.each(data.photos.photo, function(k, v) {
+            if (i < lim) {
+                var j = k + 2;
+                var src = "https://farm" + v.farm + ".staticflickr.com/" + v.server + "/" + v.id + "_" + v.secret + "_b.jpg";
+                $("#rotatingImages").append($(new Image()).attr('src', src).attr('class', 'threed').css('border', '1px solid black').css('transform', 'rotateY(' + (j * 35) + 'deg) translate3d(' + (Math.round(Math.sin(j * Math.PI / 8) * 800)) + 'px, -200px, ' + (Math.round(Math.cos(j * Math.PI / 8) * 800)) + 'px) '));
 
-                    }
+            }
 
 
 
-                    i++;
+            i++;
 
-                })
-            })
-        } else {
-            shuffle(allImg);
-            $.each(allImg, function(k, v) {
-                    if (i < lim) {
-                        var j = k + 2;
-                        var src = "https://farm" + v.farm + ".staticflickr.com/" + v.server + "/" + v.id + "_" + v.secret + "_b.jpg";
-                        $("#rotatingImages").append($(new Image()).attr('src', src).attr('class', 'threed').css('border', '1px solid black').css('transform', 'rotateY(' + (j * 35) + 'deg) translate3d(' + (Math.round(Math.sin(j * Math.PI / 8) * 800)) + 'px, -200px, ' + (Math.round(Math.cos(j * Math.PI / 8) * 800)) + 'px) '));
-
-                    }
-
-
-
-                    i++;
-
-                })
-        }
-
-
-    }
+        })
+    })
 }
 
     /* global angular, DeviceManager, RiftSandbox, Mousetrap */
