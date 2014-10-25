379af9e1f453bc50e336f4a3a408cdfbaaec82eb
bowmessage <bowmessage@gmail.com>
2014-10-25 15:02:00 -0500

    srch

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function init() {
 
     map.addLayer(mapnik);
     map.setCenter(position, zoom);
-
+/*
     var style = new OpenLayers.Style({
         pointRadius: "${radius}",
         fillColor: "#ffcc66",
@@ -61,13 +61,13 @@ function init() {
             $("#namesdiv").append("<div>" + v.data.schoolName + "</div>");
         });
 
-    }
+    }*/
 
 
     OAuth.initialize('AlrP4jjCIXkqVpJE_tZxvuqsF58')
     OAuth.popup('flickr', {}, function(error, result) {
         console.log(result)
-        result.get('services/rest/?method=flickr.photos.search&api_key=a4dbe979f03ec20953a445250a5af87f&bbox=-10%2C-10%2C10%2C10&has_geo=1&format=json&nojsoncallback=1&auth_token=72157648555864100-757f7a945585930c&api_sig=321b7a97fac8d35781d95ad69b1e066a').done(function(data) {
+        result.get('flickr.photos.search&api_key=a4dbe979f03ec20953a445250a5af87f&bbox=-10%2C-10%2C10%2C10&has_geo=1&format=json&nojsoncallback=1&auth_token=72157648555864100-757f7a945585930c&api_sig=321b7a97fac8d35781d95ad69b1e066a').done(function(data) {
             //var template = Handlebars.compile($('#entry-template').html())
             //var content = template({
             //    statuses: data.statuses
