efd83645f401e0be5c374563a52958495cb90894
bowmessage <bowmessage@gmail.com>
2014-10-25 16:43:39 -0500

    asgasdsadf

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ function init() {
             var i = 0;
             $.each(data.photos.photo, function(k, v) {
                 if (i < lim) {
-                    var src = "https://farm" + v.farm + ".staticflickr.com/" + v.server + "/" + v.id + "_" + v.secret + "_l.jpg";
+                    var src = "https://farm" + v.farm + ".staticflickr.com/" + v.server + "/" + v.id + "_" + v.secret + "_b.jpg";
                     $("#rotatingImages").append($(new Image()).attr('src', src).attr('class', 'threed').css('border', '1px solid black').css('transform', 'rotateY(20deg) translate3d('+(Math.round(Math.sin(k*Math.PI/8)*800))+'px, '+(Math.round(Math.cos(k*Math.PI/8)*800))+'px, 100px) translate(100px, 150px)'));
 
                 }
