f4bd58a8b73a4afc07cb588ee9f2534af22b7430
bowmessage <bowmessage@gmail.com>
2014-10-25 14:20:45 -0500

    rever

--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@ function init() {
 
 
     OAuth.initialize('AlrP4jjCIXkqVpJE_tZxvuqsF58')
-    OAuth.popup('twitter', {}, function(result) {
+    OAuth.popup('twitter', {}, function(error, result) {
         console.log(result)
         result.get('/1.1/search/tweets.json?q=hack&geocode=37.781157,-122.398720,100mi').done(function(data) {
             //var template = Handlebars.compile($('#entry-template').html())
