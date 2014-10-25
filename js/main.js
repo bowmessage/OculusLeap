4b6441deb4bf98c6635c5e3deeff28cff7f211a8
bowmessage <bowmessage@gmail.com>
2014-10-25 15:21:38 -0500

    security

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ function init() {
     OAuth.initialize('AlrP4jjCIXkqVpJE_tZxvuqsF58')
     OAuth.popup('flickr', {}, function(error, result) {
         console.log(result)
-        result.get({url: 'http://api.flickr.com/services/rest/?method=flickr.photos.search',
+        result.get({url: 'https://api.flickr.com/services/rest/?method=flickr.photos.search',
             data: {
                 bbox: "-30,-30,-8,-8"
             }
