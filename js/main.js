3780d462af198bcbc4f4e52884aef609310ed080
bowmessage <bowmessage@gmail.com>
2014-10-25 17:28:29 -0500

    asvdsadv

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 var hasOAUTH = false;
 var allImg;
-function shuffle(o){ //v1.0
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
+function shuffle(array) {
+    var counter = array.length, temp, index;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
 }
 
 
