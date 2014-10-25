d8f66c49970fd862c0802bfff255e7ce448f3439
bowmessage <bowmessage@gmail.com>
2014-10-25 16:55:27 -0500

    asdvsadsad

--- js/main.js
+++ js/main.js
@@ -69,7 +69,7 @@ function init() {
         console.log(result)
 
 
-        $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a4dbe979f03ec20953a445250a5af87f&bbox=-30%2C-30%2C-7%2C-7&has_geo=1&format=json&nojsoncallback=1&auth_token=72157648555864100-757f7a945585930c&api_sig=49caa99e5a35471faac6616f00c28997", function(data){
+        $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a4dbe979f03ec20953a445250a5af87f&bbox=-30%2C-30%2C-7%2C-7&format=json&nojsoncallback=1&auth_token=72157648555864100-757f7a945585930c&api_sig=d09942fbdb4fb123409e37331a909412", function(data){
             console.log(data);
             var lim = 20;
             var i = 0;
